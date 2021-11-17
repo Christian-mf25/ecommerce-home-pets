45fbabc5980e8996615a548bef230e863ee94713
Christian <christian.mf22@gmail.com>
2021-11-17 19:16:19 -0300

    fix: fixed cart erro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { GlobalStyles } from "./Styles/global";
 import Routes from "./Routes";
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <ToastContainer
         position="top-right"
         autoClose={2500}
